c0bb898914ddefa21664974bd223843a81971165
SDarkMagic <60792071+SDarkMagic@users.noreply.github.com>
2020-08-21 12:34:57 -0400

    things

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import pathlib
+
+with open("README.md", "r") as desc:
+    long_description = desc.read()
+
+
+setuptools.setup(
+    name="MapEditor++",
+    version="0.0.0a",
+    description="Ice spear, but better",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    packages=['Scripts'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "oead>=1.1.1",
+        "open3d>=0.10.0.1"
+        "pathlib"
+    ],
+)
