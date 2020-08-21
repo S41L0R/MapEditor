948adc477614ffea36fc1f968dc40ff007f4ce4d
S41L0R <mr.alexander.n.hill@gmail.com>
2020-08-21 10:46:06 -0400

    Utils

---
+++ Scripts/Utils/Utils.py
@@ -0,0 +1,17 @@
+# Decompress bymls.
+def BymlDecompress():
+    with open(Path, 'rb') as InputFile:
+        ReadInputFile = InputFile.read()
+        DeYaz0 = oead.yaz0.decompress(ReadActorInfoFile)
+        DeBYML = oead.byml.from_binary(DeYaz0)
+        output = oead.byml.to_text(DeBYML)
+        return(output)
+
+# A function for checking if a file is yaz0 compressed and then determining whether or not to decompress it based off of that
+def checkCompression(fileCheck):
+    fileInRead = fileCheck
+    if (oead.yaz0.get_header(fileInRead) is not None):
+        uncompressedFile = oead.yaz0.decompress(fileInRead)
+    else:
+        uncompressedFile = fileInRead
+    return(uncompressedFile)
