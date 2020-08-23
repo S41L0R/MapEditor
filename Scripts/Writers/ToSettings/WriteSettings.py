8467e18d311c9085aaab8755a6ec10ef0e3fb358
S41L0R <mr.alexander.n.hill@gmail.com>
2020-08-23 11:22:58 -0400

    Fixed paths

--- Scripts/Writers/ToSettings/WriteSettings.py
+++ Scripts/Writers/ToSettings/WriteSettings.py
@@ -2,5 +2,5 @@ import os
 import yaml
 
 def WriteSettings(settings):
-    with open(r'MapEditor\Config\Config.yml', 'w+') as config:
+    with open(r'..\..\..\MapEditor\Config\Config.yml', 'w+') as config:
         yaml.dump(settings, config)
