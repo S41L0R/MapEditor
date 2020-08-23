08539f6544e0f59d6c5bc44b6d359e960a277c4b
S41L0R <mr.alexander.n.hill@gmail.com>
2020-08-23 11:22:58 -0400

    Fixed paths

--- Scripts/Loaders/FromSettings/LoadSettings.py
+++ Scripts/Loaders/FromSettings/LoadSettings.py
@@ -2,7 +2,7 @@ import os
 import yaml
 
 def LoadSettings():
-    with open(r'MapEditor\Config\Config.yml', 'r') as config:
+    with open(r'..\..\..\MapEditor\Config\Config.yml', 'r') as config:
         settings = yaml.load(config, Loader=yaml.FullLoader)
         print(settings)
         return(settings)
--- Scripts/Users/Process.py
+++ Scripts/Users/Process.py
@@ -1,3 +1,8 @@
+# Add directory to sys.path
+import sys
+sys.path.append('../../../')
+
+
 # Import general stuff
 import MapEditor.Scripts.Utils.Utils as utils
 
@@ -15,7 +20,7 @@ settings = {
 try:
     settings = LoadSettings.LoadSettings()
 except:
-    WriteSettings(settings)
+    WriteSettings.WriteSettings(settings)
     print("Created and poulated yml file.")
 
 
@@ -23,11 +28,13 @@ except:
 # CREATE CODE HERE: FROM MAP FILES FIND ACTORS TO LOAD
 
 # Load Actor
+
 import MapEditor.Scripts.Loaders.FromGame.actor as actor
 
-#ActorInfoText = utils.BymlDecompress(settings["Game Dump Path"] + "/content/Actor/ActorInfo.product.sbyml")
-#print(actor.FindActorModel(ActorInfoText, "TwnObj_Village_HatenoHouseSet_A_M_01"))
-import os
+ActorInfoText = utils.BymlDecompress(settings["Game Dump Path"] + "/content/Actor/ActorInfo.product.sbyml")
+print(actor.FindActorModel(ActorInfoText, "TwnObj_Village_HatenoHouseSet_A_M_01"))
+
+# Create Window
 
 import webview
 webview.create_window('Map Editor', "../../HTML/UI.html")
