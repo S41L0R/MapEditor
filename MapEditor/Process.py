6e3f3808e62fb2f2b3534370bdcdfca287b4f4c7
SDarkMagic <60792071+SDarkMagic@users.noreply.github.com>
2020-09-20 09:01:55 -0400

    fixed it

--- MapEditor/Process.py
+++ MapEditor/Process.py
@@ -57,8 +57,8 @@ with open(pathDy, 'rb') as dataDy:
 
 staticDictOut = utils.mapDict(readFileStatic)
 dyDictOut = utils.mapDict(readFileDy)
-jsonStaticOut = json.dumps(staticDictOut, indent=2)
-jsonDyOut = json.dumps(dyDictOut, indent=2)
+jsonStaticOut = json.dumps(staticDictOut.extractedByml, indent=2)
+jsonDyOut = json.dumps(dyDictOut.extractedByml, indent=2)
 
 
 uniqueActors = utils.findUniqueActors(staticDictOut.extractedByml)
