6aa3db6243ba530fa9d6e69c09ad46f4bd3aac23
SDarkMagic <60792071+SDarkMagic@users.noreply.github.com>
2020-11-17 10:23:10 -0500

    so it turns out the code to save data is ez

--- MapEditor/Writers/ToExport/smubin.py
+++ MapEditor/Writers/ToExport/smubin.py
@@ -1,14 +1,11 @@
 import oead
 import pathlib
+import json
 import Lib.Utils.Util as util
 import Loaders.FromSettings.LoadSettings as loadSettings
 import yaml
 
 saveDir = util.get_data_dir() / 'savedEdits'
-
-class SafeLoaderIgnoreUnknown(yaml.SafeLoader):
-    def ignore_unknown(self, node):
-        return None
 
 def findBigEndian():
     settings = loadSettings.LoadSettings()
@@ -21,37 +18,15 @@ def findBigEndian():
 validMapFileExts = ['smubin', 'mubin']
 
 # Opens and reads a map file; returns a dict containing the map file data.
-def readMapFile(fileIn):
-    fileName = str(fileIn)
-    if util.extCheck(fileName, validMapFileExts) == True:
-        readMap = open(pathlib.Path(fileIn), 'rb')
-        decompMap = util.checkCompression(readMap)
-        extractMap = oead.byml.from_binary(decompMap)
-        print(extractMap)
-    else:
-        print('Invalid map file; Exiting.')
-        return
+def writeMapFile(jsonData):
+    jsonData = json.loads(jsonData)
+    sectionName = jsonData.get('Section')
+    staticMapData = jsonData.get('StaticMap')
+    dynamicMapData = jsonData.get('DynamicMap')
+    filePath = saveDir / sectionName
+    with open(pathlib.Path(f'{filePath}/{sectionName}_Static.smubin'), 'wb') as writeStatic:
+        writeStatic.write(oead.yaz0.compress(oead.byml.to_binary(staticMapData)))
+    with open(pathlib.Path(f'{filePath}/{sectionName}_Dynamic.smubin'), 'wb') as writeDynamic:
+        writeDynamic.write(oead.yaz0.compress(oead.byml.to_binary(dynamicMapData)))
+    return
 
-def FindActorText(InputText, ActorHashId):
-    # Stuff I need because there are unknown tags
-    SafeLoaderIgnoreUnknown.add_constructor(None, SafeLoaderIgnoreUnknown.ignore_unknown)
-
-    actorDict = {}
-    print(InputText)
-    for actorEntry in InputText:
-        actorDict.update(actorEntry)
-        if int(actorDict.get('HashId')) == int(ActorHashId):
-            print('Found Actor with matching hashID in inputted map file.')
-            print(oead.byml.to_text(actorDict))
-            return(actorDict)
-            actorDict.clear()
-        else:
-            actorDict.clear()
-            continue
-
-def getActors(mapIn):
-    openFile = open(pathlib.Path(mapIn), 'rb')
-    mapIn = openFile.read()
-    decompressed = util.checkCompression(mapIn)
-    binOut = oead.byml.from_binary(decompressed)
-    return(binOut)
