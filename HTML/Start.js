66c730d2fd3eb665c5563af846e56420cd1b9384
SDarkMagic <60792071+SDarkMagic@users.noreply.github.com>
2020-11-17 22:03:03 -0500

    Working on a separate menu-won't load for some reason

--- HTML/Start.js
+++ HTML/Start.js
@@ -1,8 +1,38 @@
 const { app, BrowserWindow, Menu } = require('electron')
-
+const fs = require('fs')
+const isMac = process.platform === 'darwin'
 
 const ipc = require('electron').ipcMain;
-
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {role: 'Save'},
+      {type: 'Separator'},
+      {role: 'Exit'}
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {role: 'Undo'},
+      {type: 'Separator'},
+      {role: 'Redo'},
+      {type: 'Separator'},
+      {role: 'Copy JSON'},
+      {type: 'Separator'},
+      {role: 'Paste JSON'},
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {role: 'Reload'},
+      {role: 'Force Reload'},
+      {role: "Dev-tools"}
+    ]
+  }
+];
 
 let win
 
@@ -12,7 +42,7 @@ ipc.on('toMainWindow', (event, message) => {
 })
 
 function createWindow () {
-  // Create the browser window.
+  // Create the browser window and menu
   win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -22,16 +52,20 @@ function createWindow () {
       enableRemoteModule: true
     }
   })
-
   // and load the index.html of the app.
   win.loadFile('../editor.html')
+  var menu = createMenu()
+  Menu.setApplicationMenu(menu)
   win.on('closed', () => app.quit());
 
-
-
-
-
 }
+
+function createMenu() {
+  console.warn(mainMenuTemplate)
+  const menu = Menu.buildFromTemplate(mainMenuTemplate)
+  return menu
+  }
+
 
 app.whenReady().then(createWindow)
 
