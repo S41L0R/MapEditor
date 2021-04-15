0630a0d82ee20e105c82bffc2ab8529e98d5f0ba
SDarkMagic <60792071+SDarkMagic@users.noreply.github.com>
2021-04-15 10:17:48 -0400

    auto runs setup.py install

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ import pathlib
 with open("README.md", "r") as desc:
     long_description = desc.read()
 
+with open('version.txt', 'rt') as readVer:
+    version = readVer.read()
+
 
 setuptools.setup(
     name="MapEditor",
-    version="0.0.1",
+    version=version,
     description="Ice spear, but better",
     long_description=long_description,
     long_description_content_type="text/markdown",
