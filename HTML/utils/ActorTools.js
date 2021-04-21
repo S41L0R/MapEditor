54a396d84691b9056ffdf6180d6a60b979fcf749
S41L0R <60116462+S41L0R@users.noreply.github.com>
2021-04-21 14:42:52 -0400

    Some mostly working actor update code

--- HTML/utils/ActorTools.js
+++ HTML/utils/ActorTools.js
@@ -25,6 +25,7 @@ const removeDataActors = async function(hashIDList, maplike) {
 }
 
 // Function used to remove objects from the scene or a scenelike dict via HashID.
+// Probably does not work with instancedMesh support
 const removeObjectActors = async function(hashIDList, scenelike, transformControlLike) {
 	let indexCounter = 0;
 	for (const i of scenelike.children) {
@@ -46,6 +47,86 @@ const removeObjectActors = async function(hashIDList, scenelike, transformContro
 	}
 }
 
+
+const updateDataActor = async function(dummy) {
+	let firstInstancedMesh = dummy.userData.instancedMeshes[0]
+	let instancedMeshIndex = dummy.userData.index
+	let actor = firstInstancedMesh.userData.actorList[instancedMeshIndex]
+	let actorMatrix = dummy.matrixWorld
+	let position = new global.THREE.Vector3()
+	let rotationQ = new global.THREE.Quaternion()
+	let rotationE = new global.THREE.Euler()
+	let scale = new global.THREE.Vector3()
+
+	actorMatrix.decompose(position, rotationQ, scale)
+
+	rotationE.setFromQuaternion(rotationQ)
+
+	actor.Translate[0].value = position.x
+	actor.Translate[1].value = position.y
+	actor.Translate[2].value = position.z
+
+
+	if (rotationE.x !== 0 && rotationE.y !== 0 && rotationE.z !== 0) {
+		console.error("test")
+		console.error(rotationE)
+		if (rotationE.x !== 0 && rotationE.z !== 0) {
+			console.error("test2")
+			actor.Rotate = [
+				{
+					"type": 300,
+					"value": rotationE.x
+				},
+				{
+					"type": 300,
+					"value": rotationE.y
+				},
+				{
+					"type": 300,
+					"value": rotationE.z
+				}
+			]
+		}
+		else {
+			console.error("test3")
+			actor.Rotate = {
+				"type": 300,
+				"value": rotateE.y
+			}
+		}
+	}
+
+	if (scale.x !== 1 && scale.y !== 1 && scale.z !== 1) {
+		if (scale.x !== 1 && scale.z !== 1) {
+			actor.Scale = [
+				{
+					"type": 300,
+					"value": scale.x
+				},
+				{
+					"type": 300,
+					"value": scale.y
+				},
+				{
+					"type": 300,
+					"value": scale.z
+				}
+			]
+		}
+		else {
+			actor.Scale = {
+				"type": 300,
+				"value": scale.y
+			}
+		}
+	}
+	else {
+		delete actor.scale
+	}
+
+
+}
+
 function findMeshesFromObject(object) {
 	let meshArray = [];
 	for (const i of object.children) {
@@ -63,5 +144,6 @@ function findMeshesFromObject(object) {
 module.exports = {
 	removeActors: removeActors,
 	removeDataActors: removeDataActors,
-	removeObjectActors: removeObjectActors
+	removeObjectActors: removeObjectActors,
+	updateDataActor: updateDataActor
 }
