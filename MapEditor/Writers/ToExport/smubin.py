edf526413506acc38d809adf439f76e6c2353cf7
S41L0R <60116462+S41L0R@users.noreply.github.com>
2021-05-06 12:22:29 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/S41L0R/MapEditor""
    This reverts commit b6aafec68ffaf402204d27c69594b9cd012f3ce9.

--- MapEditor/Writers/ToExport/smubin.py
+++ MapEditor/Writers/ToExport/smubin.py
@@ -20,16 +20,43 @@ validMapFileExts = ['smubin', 'mubin']
 
 # Opens and reads a map file; returns a dict containing the map file data.
 def writeMapFile(jsonData):
-    sectionName = jsonData.get('Section')
+
+    def formatMapFileData(mapData: dict):
+        formattedData = {}
+        if 'Objs' in mapData.keys():
+            objList = []
+            for entry in mapData['Objs']:
+                objList.append((entry))
+            formattedData.update({'Objs': objList})
+        else:
+            pass
+
+        if 'Rails' in mapData.keys():
+            railList = []
+            for rail in mapData['Rails']:
+                print(rail)
+                railList.append(oead.byml.Hash(rail))
+            formattedData.update({'Rails': railList})
+        else:
+            pass
+
+        print(type(formattedData))
+        return(formattedData)
+
+
+    #sectionName = jsonData.get('Section')
+    sectionName = 'J-8'
     staticMapData = util.compressByml(dict(jsonData.get('Static'))).compressedData
     dynamicMapData = util.compressByml(dict(jsonData.get('Dynamic'))).compressedData
-    print(staticMapData)
-    x = list(staticMapData)
-    print(x)
-    bigEndian = findBigEndian()
+    formattedStaticData = formatMapFileData(staticMapData)
+    bigEndian = bool(findBigEndian())
     filePath = util.findMKDir(f'{saveDir}/{sectionName}')
+    #print(staticMapData)
+    #print(type(staticMapData))
     with open(pathlib.Path(f'{filePath}/{sectionName}_Static.smubin'), 'wb') as writeStatic:
-        writeStatic.write(oead.yaz0.compress(oead.byml.to_binary(staticMapData, big_endian=bool(bigEndian))))
+        writeStatic.write(oead.yaz0.compress(oead.byml.to_binary(formattedStaticData, big_endian=bigEndian)))
+        print('saved Static')
     with open(pathlib.Path(f'{filePath}/{sectionName}_Dynamic.smubin'), 'wb') as writeDynamic:
-        writeDynamic.write(oead.yaz0.compress(oead.byml.to_binary(dynamicMapData, big_endian=bool(bigEndian))))
-    return
+        writeDynamic.write(oead.yaz0.compress(oead.byml.to_binary(oead.byml.from_text(str(dynamicMapData)), big_endian=bigEndian)))
+        print('saved Dynamic')
+    #return
